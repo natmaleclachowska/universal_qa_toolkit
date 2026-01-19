238758ed4e51924ac8f82afc272be96791442974
nat.malec-lachowska <natalia@userdata.pl>
2026-01-19 12:27:54 +0100

    feat: add stable product search test with XPath and config

---
+++ tests/test_login.py
@@ -0,0 +1,33 @@
+import pytest
+from playwright.sync_api import expect
+from config.settings import LOGIN_URL
+
+def test_successful_login(page):
+    page.goto(LOGIN_URL)
+
+    # Test 1. Poprawne zalogowanie
+    page.locator("//input[@id='username']").fill("nat.malectttt@gmail.com")
+    page.locator("//input[@id='password']").fill("TajneHaslo123!")
+    page.locator("//button[@name='login']").click()
+    expect(page.locator("//h1[@class='entry-title']")).to_be_visible()
+    print("Test 1: Poprawne zalogowanie się.")
+
+def test_failed_login_invalid_email(page):
+    page.goto(LOGIN_URL)
+
+    #Test 2. Niepoprawne logowanie (błędny adres e-mail)
+    page.locator("//input[@id='username']").fill("nat.malecttttt@gmail.com")
+    page.locator("//input[@id='password']").fill("TajneHaslo123!")
+    page.locator("//button[@name='login']").click()
+    expect(page.locator("//ul[@role='alert']")).to_be_visible()
+    print("Test 2: Logowanie nie jest możliwe (błędny adres e-mail).")
+
+def test_failed_login_invalid_password(page):
+    page.goto(LOGIN_URL)
+
+    # Test 2. Niepoprawne logowanie (błędne hasło)
+    page.locator("//input[@id='username']").fill("nat.malectttt@gmail.com")
+    page.locator("//input[@id='password']").fill("TajneHaslo1234!")
+    page.locator("//button[@name='login']").click()
+    expect(page.locator("//ul[@role='alert']")).to_be_visible()
+    print("Test 2: Logowanie nie jest możliwe (błędne hasło).")
