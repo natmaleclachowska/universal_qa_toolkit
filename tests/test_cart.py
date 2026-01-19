2c38da4378a1c04a736a4f370bac9e2dfc0a0497
nat.malec-lachowska <natalia@userdata.pl>
2026-01-19 09:54:35 +0100

    feat: add stable product search test with XPath and config

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -2,54 +2,33 @@
 DODANIA PRODUKTU DO KOSZYKA
 '''
 
-import re
-from playwright.sync_api import sync_playwright , expect
+import pytest
+from playwright.sync_api import expect
 from config.settings import BASE_URL, PRODUCT_URL
 
+def test_add_and_remove_product_from_homepage(page):
+    page.goto(BASE_URL)
+    product_in_cart = page.locator("//td//a[text()='Blue Top']")
 
-def test_add_product_to_cart_from_homepage():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto(BASE_URL)
+    # Test 1. Dodanie produktu do koszyka ze strony głównej
+    page.locator("(//a[@data-product-id='1'])").first.click()
+    page.locator("(//u[text()='View Cart'])").click()
+    expect(product_in_cart).to_be_visible()
 
-        product_in_cart = page.locator("//td//a[text()='Blue Top']")
+    # Test 2. Usunięcie produktu z koszyka
+    page.locator("(//a[@class='cart_quantity_delete'])").click()
+    expect(product_in_cart).not_to_be_visible()
 
-        # Test 1. Dodanie produktu do koszyka ze strony głównej
-        page.locator("(//a[@data-product-id='1'])").first.click() # Klikamy w pierwszy przycisk "Add to cart"
-        page.locator("(//u[text()='View Cart'])").click() # Klikamy w przycisk "View Cart"
-        page.wait_for_timeout(2000)
-        expect(product_in_cart).to_be_visible() # Produkt jest widoczny w koszyku
-        print(f"Test 1: Produkt został dodany do koszyka ze strony głównej.")
+def test_add_product_to_cart_from_details(page):
+    page.goto(PRODUCT_URL)
+    product_in_cart = page.locator("//td//a[text()='Men Tshirt']")
 
-        # Usunięcie produktu z koszyka
-        print("Rozpoczynam usunięcie produktu z koszyka")
+    # Test 3. Dodanie produktu do koszyka z karty produktu
+    page.locator("//button[contains(@class, 'cart')]").click()
+    page.locator("//u[text()='View Cart']").click()
+    expect(product_in_cart).to_be_visible()
 
-        # Test 2. Usunięcie produktu z koszyka
-        page.locator("(//a[@class='cart_quantity_delete'])").click()
-        page.wait_for_timeout(2000)
-        expect(product_in_cart).not_to_be_visible() # Produkt nie jest już widoczny w koszyku
-        print(f"Test 2: Produkt został usunięty z koszyka.")
-        browser.close()
-
-test_add_product_to_cart_from_homepage()
-
-def test_add_product_to_cart_from_details():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto(PRODUCT_URL)
-
-        product_in_cart = page.locator("//td//a[text()='Men Tshirt']")
-
-        # Test 3. Dodanie produktu do koszyka z karty produktu
-        page.locator("//button[contains(@class, 'cart')]").click()
-        page.locator("//u[text()='View Cart']").click()
-        page.wait_for_timeout(2000)
-        expect(product_in_cart).to_be_visible()
-        print(f"Test 3: Produkt został dodany do koszyka z karty produktu.")
-        browser.close()
-
-test_add_product_to_cart_from_details()
+    import re
+    import pytest
+    from playwright.sync_api import expect
+    from config.settings import BASE_URL
